e3282805c8fc166915351c50a82e497f33ceedbb
Théo Ryffel <theo.leffyr@gmail.com>
2021-06-30 16:49:24 +0200

    Fix the transformation for stats

--- loaders/pneumonia.py
+++ loaders/pneumonia.py
@@ -66,13 +66,14 @@ class PneumoniaDataset(th.utils.data.Dataset):
 
 
 def pneumonia(args):
-    stats_transform = transforms.Compose(
-        [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
+    stats_transform = AlbumentationsTorchTransform(
+        a.Compose(
+            [
+                a.Resize(args.inference_resolution, args.inference_resolution),
+                a.RandomCrop(args.train_resolution, args.train_resolution),
+                a.ToFloat(max_value=255.0),
+            ]
+        )
     )
     dataset = PneumoniaDataset(train=True, transform=stats_transform, batch_size=args.batch_size)
 
